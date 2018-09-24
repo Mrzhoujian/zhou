446dec766c5e390275bf5e341f078f066d470f0a
zhoujian <1399375734@qq.com>
2018-09-24 21:05:29 +0800

    第一次提交

---
+++ get-test-project/src/com/itcast/fist/firstclass.java
@@ -0,0 +1,10 @@
+package com.itcast.fist;
+
+public class firstclass {
+    public static void main(String[] args) {
+        System.out.println("nfeoverughqenuog");
+        System.out.println("nfeoverughqenuog");
+        System.out.println("nfeoverughqenuog");
+        System.out.println("nfeoverughqenuog");
+    }
+}
